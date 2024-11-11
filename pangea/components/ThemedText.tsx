978285b120473c67b89f69faa5351df86e1de19a
Joseph Michaut <josephmichaut@JosephMac.lan>
2024-11-11 18:44:13 +0100

    hello voici la deuxime version

--- pangea/components/ThemedText.tsx
+++ pangea/components/ThemedText.tsx
@@ -8,14 +8,6 @@ export type ThemedTextProps = TextProps & {
   type?: 'default' | 'title' | 'defaultSemiBold' | 'subtitle' | 'link';
 };
 
-export function ThemedText({
-  style,
-  lightColor,
-  darkColor,
-  type = 'default',
-  ...rest
-}: ThemedTextProps) {
-  const color = useThemeColor({ light: lightColor, dark: darkColor }, 'text');
 
   return (
     <Text
